69ac78e2c72086d45490074b140404e3528f6872
Simon <therealslimv@yahoo.de>
2018-12-05 23:41:28 +0100

    removed the pytest args

--- modules/Generator/start.py
+++ modules/Generator/start.py
@@ -1,6 +1,5 @@
 class Generator:
 
     def hello(self):
-        print("called")
         return "hello"
 
--- modules/Generator/tests/test_example.py
+++ modules/Generator/tests/test_example.py
@@ -3,5 +3,4 @@ from modules.Generator.start import Generator
 
 def test_start():
     start = Generator()
-    print("running")
     assert start.hello() == "hello"
